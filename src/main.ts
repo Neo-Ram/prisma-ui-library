10918086bffb9aca2454754fbfafa7062f0f379a
Neo-Ram <ramon.franco.reyes@gmail.com>
2025-10-02 00:58:28 -0700

    Estilos css funcionando

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,3 @@
-// Auto-importar estilos CSS
-import './Button/Button.module.css'
-import './Input/Input.module.css'
-
 // Exportar componentes
 export {Button} from './Button/Button'
 export {Input} from './Input/Input';
