6df2e66ca0ce0eedc8f49ae7d02fd04a98edef20
Neo-Ram <ramon.franco.reyes@gmail.com>
2025-10-06 12:21:19 -0700

    v1.0.1

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,14 @@
 // Exportar componentes
-export {Button} from './Button/Button'
-export {Input} from './Input/Input';
+export { Alert } from './Alert/Alert';
+export { Breadcrumb } from './Breadcrumb/Breadcrumb';
+export { Button } from './Button/Button';
+export { Checkbox } from './Checkbox/Checkbox';
+export { Input } from './Input/Input';
+export { Pagination } from './Pagination/Pagination';
+export { Radiogroup } from './Radiogroup/Radiogroup';
+export { Select } from './Select/Select';
+export { Slider } from './Slider/Slider';
+export { Spinner } from './Spinner/Spinner';
+export { Textarea } from './Textarea/Textarea';
+export { Toggle } from './Toggle/Toggle';
+export { Tooltip } from './Tooltip/Tooltip';
